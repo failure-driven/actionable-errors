9af37351d2b9734140057168dfb41b2aa9d64655
Michael Milewski <saramic@gmail.com>
2019-05-28 22:05:03 +1000

    second last run through on code

--- app/mailboxes/blog_mailbox.rb
+++ app/mailboxes/blog_mailbox.rb
@@ -1,9 +1,24 @@
 class BlogMailbox < ApplicationMailbox
   def process
-    post = Post.create!(title: mail.subject, text: mail.parts.find{|p| p.content_type =~/text/ }.decode_body)
-    # decoded_data = mail.parts.find{|p| p.content_type =~/image/ }.decode_body
-    # File.open("#{Rails.root}/tmp/demo.jpg", 'wb'){|f| f.write(decoded_data) }
-    # post.image.attach(io: File.open("#{Rails.root}/tmp/demo.jpg"), filename: 'file.jpg')
-    # post.save!
+    text = text_part(mail)
+    post = Post.create!(title: mail.subject, text: text)
+    decoded_image = decoded_image_part(mail)
+    attach_image(post, decoded_image) if decoded_image
+  end
+
+  private
+
+  def text_part(mail)
+    mail.parts.find{|part| part.content_type =~ /text/ }&.decode_body || mail.decode_body
+  end
+
+  def decoded_image_part(mail)
+    mail.parts.length > 1 && mail.parts.find{|part| part.content_type =~ /image/ }.decode_body
+  end
+
+  def attach_image(model, decoded_image)
+    File.open("#{Rails.root}/tmp/demo.jpg", 'wb'){|f| f.write(decoded_image) }
+    model.image.attach(io: File.open("#{Rails.root}/tmp/demo.jpg"), filename: 'file.jpg')
+    model.save!
   end
 end
