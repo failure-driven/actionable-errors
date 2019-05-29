7777967b65bd3e36b0364f478f5238b6ab6540da
Michael Milewski <saramic@gmail.com>
2019-05-29 10:17:51 +1000

    use StringIO to stream some bytes to an attachment
    Co-authored-by: Dave McDonald <davesaddress.nz@gmail.com>

--- app/mailboxes/blog_mailbox.rb
+++ app/mailboxes/blog_mailbox.rb
@@ -3,7 +3,8 @@ class BlogMailbox < ApplicationMailbox
     text = text_part(mail)
     post = Post.create!(title: mail.subject, text: text)
     decoded_image = decoded_image_part(mail)
-    attach_image(post, decoded_image) if decoded_image
+    post.image.attach(io: StringIO.new(decoded_image), filename: 'the_file.jpg')
+    post.save!
   end
 
   private
@@ -14,11 +15,5 @@ class BlogMailbox < ApplicationMailbox
 
   def decoded_image_part(mail)
     mail.parts.length > 1 && mail.parts.find{|part| part.content_type =~ /image/ }.decode_body
-  end
-
-  def attach_image(model, decoded_image)
-    File.open("#{Rails.root}/tmp/demo.jpg", 'wb'){|f| f.write(decoded_image) }
-    model.image.attach(io: File.open("#{Rails.root}/tmp/demo.jpg"), filename: 'file.jpg')
-    model.save!
   end
 end
